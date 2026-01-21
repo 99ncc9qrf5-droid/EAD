e8292315480c54ca83da304879de719936829de7
nadeeravishwashanthilakarathna <nadeeravishwashanthilakarathna@localhost>
2026-01-21 12:23:17 +0530

    initial commit

---
+++ src/dse25/pkg1ead/DSE251EAD.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package dse25.pkg1ead;
+
+/**
+ *
+ * @author nadeeravishwashanthilakarathna
+ */
+public class DSE251EAD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
